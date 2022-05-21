1b962975e7fc36ba42522c716a8af4a85bda3d59
jamalmatic <jamalmatic@gmail.com>
2022-05-21 16:09:22 +0100

    example

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Alert from "components/Alert/Alert"
 
 export default function Home() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col space-y-5 max-w-7xl mx-auto items-center justify-center">
       <Alert/>
       <Alert variant="info"/>
       <Alert variant="success"/>
       <Alert variant="warning"/>
       <Alert variant="danger"/>
-    </>
+    </div>
   )
 }
 
